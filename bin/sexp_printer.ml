2dd98ba17c53fae70c04e7fbda9ee962cc0309b5
Scander Mustapha <mustpaha.ziy@gmail.com>
2024-03-06 03:46:20 -0500

    prepend newline

--- bin/sexp_printer.ml
+++ bin/sexp_printer.ml
@@ -1,10 +1,11 @@
 open Base
 open Sexplib
-open Stdio
 open Stdio.In_channel
 
 let config = Sexp_pretty.Config.default
+let prepend_newline s = "\n" ^ s
 
 let () =
   stdin |> input_all |> Sexp.of_string
-  |> Sexp_pretty.pp_out_channel config stdout
+  |> Sexp_pretty.pretty_string config
+  |> prepend_newline |> Stdio.Out_channel.print_string
